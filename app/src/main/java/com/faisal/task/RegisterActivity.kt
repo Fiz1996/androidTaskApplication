97a7234354c01158dbed1cfd67fe89218a8a496e
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2023-01-21 23:36:54 +0300

    added validation when number is already registered

--- app/src/main/java/com/faisal/task/RegisterActivity.kt
+++ app/src/main/java/com/faisal/task/RegisterActivity.kt
@@ -2,11 +2,19 @@ package com.faisal.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     private val database = UserDatabase(this)
+    private fun checkPhoneNumber(phoneNumber:String): Boolean {
+
+        val d = database.checkPhoneNumber(phoneNumber)
+        Log.d("TAG", " check number value is: $d")
+        return d <= 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_acitivity)
@@ -17,7 +25,20 @@ class RegisterActivity : AppCompatActivity() {
 
 
         register.setOnClickListener {
-            database.insertUser(name.text.toString(),phoneNumber.text.toString(),email.text.toString())
+            val isPhoneNumberValid = checkPhoneNumber(phoneNumber.text.toString())
+
+            when {
+                isPhoneNumberValid -> {
+                    database.insertUser(
+                        name.text.toString(),
+                        phoneNumber.text.toString(),
+                        email.text.toString()
+                    )
+                }
+             !isPhoneNumberValid -> Toast.makeText(this,"this phone number has been registered"+
+             "please go back to the login page",Toast.LENGTH_SHORT).show()
+            }
+
             finish()
         }
 
