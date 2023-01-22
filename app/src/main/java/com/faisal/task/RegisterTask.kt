8a1d6e7fafd1df242bf793c04f55199ec8766751
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2023-01-22 09:38:54 +0300

    added radio button for is completed tasks

--- app/src/main/java/com/faisal/task/RegisterTask.kt
+++ app/src/main/java/com/faisal/task/RegisterTask.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 
 class RegisterTask : AppCompatActivity() {
@@ -12,18 +13,14 @@ class RegisterTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_task)
-        val isCompleted = findViewById<EditText>(R.id.is_completed_text)
+        val isCompleted = findViewById<RadioButton>(R.id.isCompleted)
+        val radioButton = isCompleted.isChecked
         val taskName = findViewById<EditText>(R.id.task_name_text)
         val register = findViewById<Button>(R.id.register_task)
         val phoneNumber: String? = intent.getStringExtra("phoneNumber")
         register.setOnClickListener{
 
-            var taskIsCompleted = false
-            if ( isCompleted.text.toString().equals("done")) {
-                taskIsCompleted =true
-            }
-
-            database.insertTask(taskName.text.toString(),taskIsCompleted,phoneNumber)
+            database.insertTask(taskName.text.toString(),radioButton,phoneNumber)
             finish()
         }
 
